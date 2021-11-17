52ed967634b23630aee28faef76a5ec8b7aa2f78
Arthur Neves <arthurnvsm@gmail.com>
2021-11-17 10:57:46 -0300

    first commit

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by Arthur Neves on 17/11/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
