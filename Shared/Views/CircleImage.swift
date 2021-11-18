d9e48dcad49dfab4f697e82c0cb9a0cd65384be0
Arthur Neves <arthurnvsm@gmail.com>
2021-11-18 14:32:07 -0300

    Building Lists and Navigation

--- Shared/Views/CircleImage.swift
+++ Shared/Views/CircleImage.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 struct CircleImage: View {
+  var image: Image
+  
   var body: some View {
-    Image("turtlerock")
-//      .resizable()
-//      .scaledToFit()
+    image
       .clipShape(Circle())
       .overlay {
         Circle().stroke(.white, lineWidth: 4)
@@ -22,7 +22,7 @@ struct CircleImage: View {
 
 struct CircleImage_Previews: PreviewProvider {
   static var previews: some View {
-    CircleImage()
+    CircleImage(image: landmarks[0].image)
       .previewLayout(.sizeThatFits)
       .padding()
   }
--- Shared/Views/LandmarkList.swift
+++ Shared/Views/LandmarkList.swift
@@ -12,7 +12,7 @@ struct LandmarkList: View {
   var body: some View {
     NavigationView {
       List (landmarks) { landmark in
-        NavigationLink(destination: LandMarkDetailView()) {
+        NavigationLink(destination: LandMarkDetail(landmark: landmark)) {
           LandmarkRow(landmark: landmark)
         }
       }
@@ -23,6 +23,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
   static var previews: some View {
-    LandmarkList()
+    ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+      LandmarkList()
+        .previewDevice(PreviewDevice(rawValue: deviceName))
+        .previewDisplayName(deviceName)
+    }
   }
 }
