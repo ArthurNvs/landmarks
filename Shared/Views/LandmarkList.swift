c4c14a439430ab59512e18301babb78f436b05bf
Arthur Neves <arthurnvsm@gmail.com>
2021-11-18 11:15:30 -0300

    Landmark Detail View

--- Shared/Views/LandmarkList.swift
+++ Shared/Views/LandmarkList.swift
@@ -10,9 +10,14 @@ import SwiftUI
 struct LandmarkList: View {
   
   var body: some View {
-    List (landmarks) { landmark in
-      LandmarkRow(landmark: landmark)
-    }
+    NavigationView {
+      List (landmarks) { landmark in
+        NavigationLink(destination: LandMarkDetailView()) {
+          LandmarkRow(landmark: landmark)
+        }
+      }
+      .navigationTitle("Landmarks")
+    }//: Navigation
   }
 }
 
